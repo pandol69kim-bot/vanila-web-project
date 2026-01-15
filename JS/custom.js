6ffc58f350b88a7f76ca8d539a62de484e0c1d62
pandolkim <pandol69.kim@gmail.com>
2026-01-15 19:49:32 +0900

    feature section complete

--- JS/custom.js
+++ JS/custom.js
@@ -1,16 +1,3 @@
-/** Header Chagne Effect **/
-const stickyHeader = () => {
-  const header = document.querySelector('#header');
-  const scry = window.scrollY;
-  if (scry > 0) {
-    header.classList.add('active');
-  } else {
-    header.classList.remove('active');
-  }
-};
-
-window.addEventListener('scroll', stickyHeader);
-
 /** Scroll Reveal Effect**/
 const sr = ScrollReveal({
   reset: false, // 스크롤 올렸을때 다시 실해 여부
@@ -19,6 +6,21 @@ const sr = ScrollReveal({
 sr.reveal('.landing-text-box', {
   direction: 1000,
   origin: 'right',
-  distance: '50px',
-  duration: 3000,
+  distance: '80px',
+  duration: 1000,
+});
+
+sr.reveal('.meet-text-box', {
+  direction: 1000,
+  origin: 'bottom',
+  distance: '40px',
+  duration: 1000,
+});
+
+sr.reveal('.meet-image-box img, .feature', {
+  direction: 1000,
+  origin: 'bottom',
+  distance: '40px',
+  interval: 200, //0.2초 간격으로 차례로 효과진행
+  duration: 1000,
 });
---
+++ JS/header.js
@@ -0,0 +1,36 @@
+/** Header Chagne Effect **/
+const stickyHeader = () => {
+  const header = document.querySelector('#header');
+  const scry = window.scrollY;
+  if (scry > 0) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+};
+
+window.addEventListener('scroll', stickyHeader);
+
+/** Menu Elements Clone for Mobile **/
+const mobileMenu = document.querySelector('.mobile-menus');
+const navs = document.querySelector('.nav-lists').cloneNode(true);
+const info = document.querySelector('.info').cloneNode(true);
+mobileMenu.appendChild(navs);
+mobileMenu.appendChild(info);
+
+/** Mobile Menu Toggle **/
+const mobileBtn = document.querySelector('.mobile-btn');
+const menuHeight = mobileMenu.scrollHeight;
+
+toggleMobileBtn = (e) => {
+  const target = e.currentTarget;
+  target.classList.toggle('active');
+  if (target.classList.contains('active')) {
+    target.classList.remove('not-active');
+    mobileMenu.style.height = menuHeight + 'px';
+  } else {
+    target.classList.add('not-active');
+    mobileMenu.style.height = 0;
+  }
+};
+mobileBtn.addEventListener('click', toggleMobileBtn);
